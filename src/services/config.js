7c56be14690f9955411a0113a48f65f10a0471b8
longpt1103 <longpt1103@gmail.com>
2024-06-10 13:01:53 +0700

    fix api

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import { STEAM_API_KEY } from 'utils/constants'
 
-const steamProxy = 'https://api.steampowered.com'
+const steamProxy = 'https://steam-proxy.gplay.vn'
 
 const config = {
   community: 'https://graph.gplay.vn/community/api/v2.0',
