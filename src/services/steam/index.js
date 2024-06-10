e5d6f9cd857f768e0c60270af195b12ae4b51f93
Phat <nmphat0912@gmail.com>
2024-06-10 20:07:50 +0700

    Fix: add filter game appId

--- src/services/steam/index.js
+++ src/services/steam/index.js
@@ -1,13 +1,14 @@
 import { axiosCancel as axiosService } from 'services/axios'
 import baseUrl from 'services/baseUrl'
-import { FETCH_SERVER_LIST_ALL } from 'utils/constants'
+import { FETCH_SERVER_LIST_ALL, GAME_APP_ID } from 'utils/constants'
 
 export const getServerList = async ({
-  filters = [],
+  filters: filtersGame = [],
   limit = FETCH_SERVER_LIST_ALL,
   ...rest
 }) => {
   const s = '\\' // \ char
+  const filters = [{ key: 'appid', value: GAME_APP_ID }, ...filtersGame]
   const filterString = filters.reduce((line, filter) => {
     line += `${s}${filter.key}${s}${filter.value}`
     return line
